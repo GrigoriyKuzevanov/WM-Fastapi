983d990ce9fc137ff70e2dfdd24765ee11acf15f
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-11-29 15:19:14 +0500

    add spimex trade result model.

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ("db_connector", "Base")
+__all__ = ("db_connector", "Base", "SpimexTradeResult")
 
 from .base import Base
 from .db_connector import db_connector
+from .trade_result import SpimexTradeResult
