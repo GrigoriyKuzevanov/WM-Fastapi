b877e043e2817138ae8aa5fd0ab28ac75bc076da
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-11-29 14:22:32 +0500

    add application shutdown event - database dispose.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,32 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
 import uvicorn
 from fastapi import FastAPI
 
 from api import api_router
 from core.config import settings
+from core.models import db_connector
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    """Manages the fastapi application's lifespan handling startup and shutdown events.
+
+    Args:
+        app (FastAPI): The FastAPI application instance
+
+    Returns:
+        AsyncGenerator[None, None]: AsyncGenerator using by FastAPI
+    """
+
+    yield
+    await db_connector.dispose()
+
+
+app = FastAPI(
+    lifespan=lifespan,
+)
 app.include_router(
     api_router,
     prefix=settings.api.prefix,
