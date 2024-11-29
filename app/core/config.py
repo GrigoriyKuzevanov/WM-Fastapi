00bbdd35dab8149b521c586ce22fb4086f97ac58
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-11-29 12:11:59 +0500

    add database settings.

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn, computed_field
+from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings
 
 
@@ -28,18 +29,66 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class PostgresDBConfig(BaseModel):
+    """A class for database connection settings.
+
+    Attributes:
+        scheme (str): Scheme for build postgresdsn. "postgresql+asycnpg" by default
+        db_user (str): Database user's name to connect
+        db_password (str): Database user's password to connect
+        db_host (str): Host to connect to database
+        db_port (int): Port to connect to database
+        db_name (str): Database's name to connect
+        echo_sql (bool): Logging sql statesments. "True" by default
+        echo_pool (bool): Logging connection pool information. "True" by default
+        pool_size (int): The number of connections to keep open inside the connection
+        pool. 40 by default
+        max_overflow (int): The number of connections to allow in connection pool
+        overflow. 10 by default
+
+    Properties:
+        postgres_url (PostgresDsn): Postgres url built from db settings.
+    """
+
+    scheme: str = "postgresql+asycnpg"
+    db_user: str
+    db_password: str
+    db_host: str
+    db_port: int
+    db_name: str
+    echo_sql: bool = True
+    echo_pool: bool = True
+    pool_size: int = 40
+    max_overflow: int = 10
+
+    @computed_field
+    @property
+    def postgres_url(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme=self.scheme,
+            username=self.db_user,
+            password=self.db_password,
+            host=self.db_host,
+            port=self.db_port,
+            path=self.db_name,
+        )
+
+
 class Settings(BaseSettings):
     """A base class for app's settings. Settings values might be overriden by environmet
     variables.
 
     Attributes:
-        run (RunConfig): RunConfig class's instance of settings for running the main
+        run (RunConfig): RunConfig class's instance with settings for running the main
         application
-        api (ApiPrefix): ApiPrefix class's instance of settings for api prefix
+        api (ApiPrefix): ApiPrefix class's instance with settings for api prefix
+        main_pg_db (PostgresDBConfig): PostgresDBConfig class's instance with settings
+        for database
     """
 
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    main_pg_db: PostgresDBConfig
 
 
 settings = Settings()
