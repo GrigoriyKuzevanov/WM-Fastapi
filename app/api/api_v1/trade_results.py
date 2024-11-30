89dda5755c62e058466862dd789a3696846ec9e8
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-11-30 15:36:14 +0500

    create fastapi path function to get all trade results.

---
+++ app/api/api_v1/trade_results.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.api_v1.crud import read_all_trade_results
+from core.config import settings
+from core.models import db_connector
+from core.schemas import SpimexTradeResultBase
+
+router = APIRouter(prefix=settings.api.v1.trade_results, tags=["Trade-results"])
+
+
+@router.get("", response_model=list[SpimexTradeResultBase])
+async def get_trade_results(
+    limit: int = 10,
+    skip: int = 0,
+    session: AsyncSession = Depends(db_connector.get_session),
+) -> list[SpimexTradeResultBase]:
+    trade_results = await read_all_trade_results(limit, skip, session)
+
+    return trade_results
