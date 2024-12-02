aaf558a22eb36e4ed3093cd82b569602ab2afb7d
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-02 17:35:28 +0500

    fix docstrings.

--- app/api/api_v1/crud/trade_results.py
+++ app/api/api_v1/crud/trade_results.py
@@ -19,6 +19,9 @@ async def read_all_trade_results(
     Args:
         limit (int): The maximum number of records to retrieve.
         skip (int): The number of records to skip in the database
+        oil_id (str): Oil id filter parameter
+        delivery_type_id (str): delivery type id filter parameter
+        delivery_basis_id (str): delivery basis id filter parameter
         session (AsyncSession): The async database session's instance
 
     Returns:
@@ -62,7 +65,14 @@ async def read_last_trading_dates(
 
 
 async def read_dynamics(
-    start_date: datetime.date, end_date: datetime.date, session: AsyncSession
+    start_date: datetime.date,
+    end_date: datetime.date,
+    oil_id: str,
+    delivery_type_id: str,
+    delivery_basis_id: str,
+    limit: int,
+    skip: int,
+    session: AsyncSession,
 ) -> list[trade_result_model]:
     """Fetches a list of trade results from the database within the specified date
     range.
@@ -70,6 +80,11 @@ async def read_dynamics(
     Args:
         start_date (datetime.date): The start date of quering period
         end_date (datetime.date): The end date of quering period
+        limit (int): The maximum number of records to retrieve.
+        skip (int): The number of records to skip in the database
+        oil_id (str): Oil id filter parameter
+        delivery_type_id (str): delivery type id filter parameter
+        delivery_basis_id (str): delivery basis id filter parameter
         session (AsyncSession): The async database session's instance
 
     Returns:
@@ -79,6 +94,16 @@ async def read_dynamics(
     stmt = select(trade_result_model).filter(
         trade_result_model.date <= end_date, trade_result_model.date >= start_date
     )
+
+    if oil_id:
+        stmt = stmt.filter(trade_result_model.oil_id == oil_id)
+    if delivery_type_id:
+        stmt = stmt.filter(trade_result_model.delivery_type_id == delivery_type_id)
+    if delivery_basis_id:
+        stmt = stmt.filter(trade_result_model.delivery_basis_id == delivery_basis_id)
+
+    stmt = stmt.offset(skip).limit(limit)
+
     db_results = await session.scalars(stmt)
 
     return db_results.all()
