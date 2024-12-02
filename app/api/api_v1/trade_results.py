7328014f80b9945a04c7a8392b78b05f24a37489
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-02 17:36:01 +0500

    add filter query parameters to get dynamics path function.

--- app/api/api_v1/trade_results.py
+++ app/api/api_v1/trade_results.py
@@ -1,4 +1,3 @@
-import datetime
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, Query
@@ -11,7 +10,7 @@ from api.api_v1.crud import (
 )
 from core.config import settings
 from core.models import db_connector
-from core.schemas import TradeResultOut, TradingFilterParams
+from core.schemas import DynamicsFilterParams, TradeResultOut, TradingFilterParams
 
 router = APIRouter(prefix=settings.api.v1.trade_results, tags=["Trade-results"])
 
@@ -47,10 +46,18 @@ async def get_last_trading_dates(
 
 @router.get("/dynamics", response_model=list[TradeResultOut])
 async def get_dynamics(
-    start_date: datetime.date,
-    end_date: datetime.date,
+    filter_query: Annotated[DynamicsFilterParams, Query()],
     session: AsyncSession = Depends(db_connector.get_session),
 ) -> list[TradeResultOut]:
-    trade_results = await read_dynamics(start_date, end_date, session)
+    trade_results = await read_dynamics(
+        start_date=filter_query.start_date,
+        end_date=filter_query.end_date,
+        oil_id=filter_query.oil_id,
+        delivery_type_id=filter_query.delivery_type_id,
+        delivery_basis_id=filter_query.delivery_basis_id,
+        limit=filter_query.limit,
+        skip=filter_query.skip,
+        session=session,
+    )
 
     return trade_results
