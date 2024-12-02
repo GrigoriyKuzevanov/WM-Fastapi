7fb07c954de013f06717f2cb275e04ac2cf1a86b
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-02 14:40:07 +0500

    add path function for reading trade dynamics.

--- app/api/api_v1/trade_results.py
+++ app/api/api_v1/trade_results.py
@@ -1,7 +1,13 @@
+import datetime
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.api_v1.crud import read_all_trade_results, read_last_trading_dates
+from api.api_v1.crud import (
+    read_all_trade_results,
+    read_dynamics,
+    read_last_trading_dates,
+)
 from core.config import settings
 from core.models import db_connector
 from core.schemas import TradeResultOut
@@ -30,3 +36,14 @@ async def get_last_trading_dates(
     response = [trade_date.strftime("%d-%m-%Y") for trade_date in trade_dates]
 
     return response
+
+
+@router.get("/dynamics", response_model=list[TradeResultOut])
+async def get_dynamics(
+    start_date: datetime.date,
+    end_date: datetime.date,
+    session: AsyncSession = Depends(db_connector.get_session),
+) -> list[TradeResultOut]:
+    trade_results = await read_dynamics(start_date, end_date, session)
+
+    return trade_results
