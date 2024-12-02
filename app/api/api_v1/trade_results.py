e168e717884fb9af07a2644331d97745e1807e0f
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-02 13:59:29 +0500

    add path for getting last trade dates.

--- app/api/api_v1/trade_results.py
+++ app/api/api_v1/trade_results.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.api_v1.crud import read_all_trade_results
+from api.api_v1.crud import read_all_trade_results, read_last_trading_dates
 from core.config import settings
 from core.models import db_connector
 from core.schemas import TradeResultOut
@@ -18,3 +18,15 @@ async def get_trade_results(
     trade_results = await read_all_trade_results(limit, skip, session)
 
     return trade_results
+
+
+@router.get("/last-dates")
+async def get_last_trading_dates(
+    days: int = 1,
+    session: AsyncSession = Depends(db_connector.get_session),
+) -> list[str]:
+    trade_dates = await read_last_trading_dates(days, session)
+
+    response = [trade_date.strftime("%d-%m-%Y") for trade_date in trade_dates]
+
+    return response
