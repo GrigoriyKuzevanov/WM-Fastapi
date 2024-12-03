f71f5d72d5e11117453e40899539e9e5423c9a93
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-03 16:54:20 +0500

    add clearing cache task.

--- app/core/redis/__init__.py
+++ app/core/redis/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ("get_redis_cache", "request_key_builder")
+__all__ = ("get_redis_cache", "request_key_builder", "clear_cache_task")
 
 from .redis_cache import get_redis_cache
+from .redis_tasks import clear_cache_task
 from .request_key_builder import request_key_builder
