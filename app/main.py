a47ccaafb5febccfb3b8ef224cc8ca2bb5bb4bbe
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-03 14:35:22 +0500

    add redis cache init to start up of Fastapi lifespan function.

--- app/main.py
+++ app/main.py
@@ -3,15 +3,21 @@ from typing import AsyncGenerator
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 
 from api import api_router
 from core.config import settings
 from core.models import db_connector
+from core.redis import get_redis_cache
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     """Manages the fastapi application's lifespan handling startup and shutdown events.
+
+    on startup: inits redis for FastAPICache with redis backend
+    on shutdown: closes database connection
 
     Args:
         app (FastAPI): The FastAPI application instance
@@ -20,7 +26,11 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
         AsyncGenerator[None, None]: AsyncGenerator using by FastAPI
     """
 
+    redis = await get_redis_cache()
+    FastAPICache.init(RedisBackend(redis), prefix="main-cache")
+
     yield
+
     await db_connector.dispose()
 
 
