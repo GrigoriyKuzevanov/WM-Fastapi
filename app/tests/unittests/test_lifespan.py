b7649d78c2356dbc1abd16ee9fbccad038fb595b
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-09 14:28:29 +0500

    fix lifespan testing function.

--- app/tests/unittests/test_lifespan.py
+++ app/tests/unittests/test_lifespan.py
@@ -13,14 +13,12 @@ async def test_lifespan():
     mock_redis = MagicMock()
     mock_fastapi_cache = MagicMock()
     mock_redis_backend = MagicMock()
-    mock_scheduler = MagicMock()
     mock_db_connector = AsyncMock()
 
     with (
         patch("core.lifespan.redis_client.get_client", return_value=mock_redis),
         patch("core.lifespan.RedisBackend", return_value=mock_redis_backend),
         patch("core.lifespan.FastAPICache.init", mock_fastapi_cache.init),
-        patch("core.lifespan.scheduler", mock_scheduler),
         patch("core.lifespan.db_connector", mock_db_connector),
     ):
         app = FastAPI(lifespan=lifespan)
@@ -29,8 +27,5 @@ async def test_lifespan():
             mock_fastapi_cache.init.assert_called_once_with(
                 mock_redis_backend, prefix="main-cache"
             )
-            mock_scheduler.add_job.assert_called_once()
-            mock_scheduler.start.assert_called_once()
 
-        mock_scheduler.shutdown.assert_called_once()
         mock_db_connector.dispose.assert_awaited_once()
