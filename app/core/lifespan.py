da7222d5e1bb1489604587e024c2438d544e99e4
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-09 14:28:12 +0500

    Remove clear cache task.

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -1,17 +1,12 @@
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
 from fastapi import FastAPI
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 
-from core.config import settings
 from core.models import db_connector
-from core.redis import clear_cache_task, redis_client
-
-scheduler = AsyncIOScheduler()
+from core.redis import redis_client
 
 
 @asynccontextmanager
@@ -20,10 +15,8 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
 
     on startup:
         1) inits redis for FastAPICache with redis backend
-        2) adds job for clearing cache and starts apscheduler
     on shutdown:
-        1) shuts down apscheduler
-        2) closes database connection
+        1) closes database connection
 
     Args:
         app (FastAPI): The FastAPI application instance
@@ -35,20 +28,6 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     redis = redis_client.get_client()
     FastAPICache.init(RedisBackend(redis), prefix="main-cache")
 
-    scheduler.add_job(
-        clear_cache_task,
-        CronTrigger(
-            hour=settings.clear_cache.hour,
-            minute=settings.clear_cache.minute,
-            timezone=settings.clear_cache.timezone,
-        ),
-        id="crear_cache_job",
-        replace_existing=True,
-    )
-    scheduler.start()
-
     yield
-
-    scheduler.shutdown()
 
     await db_connector.dispose()
--- app/core/redis/__init__.py
+++ app/core/redis/__init__.py
@@ -1,5 +1,4 @@
-__all__ = ("request_key_builder", "clear_cache_task", "redis_client")
+__all__ = ("request_key_builder", "redis_client")
 
 from .redis_cache import redis_client
-from .redis_tasks import clear_cache_task
 from .request_key_builder import request_key_builder
