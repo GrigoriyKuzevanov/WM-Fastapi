50c78be590e4229a3a44160db1a295a789aa83fd
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-09 11:20:29 +0500

    add RedisClient class to manage redis connection.

--- app/core/redis/redis_cache.py
+++ app/core/redis/redis_cache.py
@@ -3,11 +3,26 @@ from redis.asyncio import Redis, from_url
 from core.config import settings
 
 
-def get_redis_cache() -> Redis:
-    """Connects to Redis using url from settings.
+class RedisClient:
+    """A class to manage connection to Redis database."""
 
-    Returns:
-        Redis: Redis client to connect.
-    """
+    def __init__(self, url: str) -> None:
+        """Inits Redis clients from given url to conntction.
 
-    return from_url(settings.redis_cache.redis_url.unicode_string())
+        Args:
+            url (str): URL to connect to redis
+        """
+
+        self.client = from_url(url)
+
+    def get_client(self) -> Redis:
+        """Returns Redis client to connect.
+
+        Returns:
+            Redis: Redis client to connect
+        """
+
+        return self.client
+
+
+redis_client = RedisClient(settings.redis_cache.redis_url.unicode_string())
